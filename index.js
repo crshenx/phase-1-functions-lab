de7c01ec14dfba32986453a72487dcd719490172
Chris Hendricks <crshenx@gmail.com>
2022-06-06 17:08:41 -0700

    commit tree 1bd6d1d8380913a068b0dad6aac2bcd3166f2de2

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(pickUpLocation) {
+  return Math.abs(pickUpLocation - 42);
+}
+
+function distanceFromHqInFeet(someValue) {
+  return 264 * distanceFromHqInBlocks(someValue);
+  // call the distanceFromHqInBlocks function from inside the distanceFromHqInFeet function,
+  // passing the argument from distanceFromHqInFeet into distanceFromHqInBlocks
+
+  // the return value of distanceFromHqInBlocks can then be used to calculate feet
+}
+
+function distanceTravelledInFeet(start, destination) {
+  //returns the number of feet traveled
+  return Math.abs(start - destination) * 264;
+}
+
+function calculatesFarePrice(start, destination) {
+  //returns the fare for the customer
+  let distance = distanceTravelledInFeet(start, destination);
+  if (distance < 400) {
+    return 0;
+  } else if (distance < 2000) {
+    let price = (distance * 0.02 - 8).toFixed(2);
+    return parseFloat(price);
+  } else if (distance < 2500) {
+    return 25;
+  }
+  return "cannot travel that far";
+}
